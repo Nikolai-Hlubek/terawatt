24c3d2275add7886458de15fbd059410d78e8cad
Frank Dressel <vagrant@debian-jessie.vagrantup.com>
2016-09-22 08:41:25 +0000

    Moved model parts to basics.

--- terawatt_model/basics.py
+++ terawatt_model/basics.py
@@ -61,3 +61,57 @@ class Device:
         self.power.thermal = power.thermal
         self.power.chemical = power.chemical
         self.power.solar = power.solar
+
+class Need:
+    ''' This class represents the need for power at a given timepoint in the future. '''
+
+    def __init__(self, time, power):
+        # Future timepoint in seconds since 01.01.1970
+        self.timepoint=time
+        # Power needed in W
+        self.power=power
+
+class Consumer:
+    ''' This class represents an entity which consumes energy at a certain point in 
+    the future. This means that random or very short term power consumption will not
+    be handled here.'''
+
+    def __init__(self, broker):
+        self.need=[]
+
+    def update():
+        __updateNeed()
+        __tellBroker()
+
+    def __updateNeed(self):
+        pass
+
+    def __tellBroker(self):
+        broker.update(self.need)
+
+class Producer:
+    ''' This class represents an entity which is able to produce energy at a certain
+    point in the future. '''
+
+    def __init__(self):
+        self.registeredNeeds=[]
+
+    def providePower(self, need):
+        remaining_need,available_need=__provideEnergy(need)
+        __registerNeed(available_need)
+        return remaining_need
+
+    def update(self):
+        pass
+
+    def __provideEnergy(self, need):
+        pass
+
+    def _registerNeed(self, available_need):
+        pass
+
+class Broker:
+    ''' The broker receives all energy needs and sends requests to producers. In general,
+    this could be parallelized?'''
+    def __init__(self):
+        pass
